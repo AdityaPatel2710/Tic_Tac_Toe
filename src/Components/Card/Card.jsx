31b9f2da4271fd840787132338642207017b2495
AdityaPatel2710 <ap8154036084@gmail.com>
2025-06-26 14:41:24 +0530

    Added basic player's turn feature

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import Icon from "../Icon/Icon";
 import "./Card.css";
 
-function Card({ type }) {
+function Card({ onPlayMove }) {
+    const[type, setType] = useState("");
+
+    function playMove() {
+        if(onPlayMove()) setType("circle");
+        else setType("cross");
+    }
 
     return (
-        <div className="card">
+        <div className="card" onClick={(type == "") ? playMove : null} >
             <div className="icon">
                 <Icon iconName = {type}/>
             </div>
--- src/Components/Grid/Grid.jsx
+++ src/Components/Grid/Grid.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import Card from "../Card/Card";
 import "./Grid.css";
 
 function Grid({ numberOfCards }) {
+    const [turn, setTurn] = useState(true); // true -> O, false -> X
+
+    function playMove() {
+        setTurn(!turn);  // value changed after playMove function is executed
+        return turn;  // return curr turn value (value of turn before changing)
+    }
+
     return (
-        <div className="grid">
-            {
-                Array(numberOfCards).fill().map((el, idx) => {
-                    return <Card key={idx} type="" />
-                })
-            }
-        </div>
+        <>
+            <div className="turn-header"> Current Turn : <span> {(turn) ? 'O' : 'X'} </span> </div>
+            <div className="grid">
+                {
+                    Array(numberOfCards).fill().map((el, idx) => {
+                        return <Card key = {idx} onPlayMove = {playMove} />
+                    })
+                }
+            </div>
+        </>
     )
 }
 
